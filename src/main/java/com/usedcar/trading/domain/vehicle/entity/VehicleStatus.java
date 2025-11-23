0d9862470d858f6350d147271493a6b7b458ac20
Jhui91 <jinso0911@naver.com>
2025-11-24 03:43:05 +0900

    Feat: 매물 유효기간 만료 처리를 위한 상태(EXPIRED) 추가

--- src/main/java/com/usedcar/trading/domain/vehicle/entity/VehicleStatus.java
+++ src/main/java/com/usedcar/trading/domain/vehicle/entity/VehicleStatus.java
@@ -1,5 +1,5 @@
 package com.usedcar.trading.domain.vehicle.entity;
 
 public enum VehicleStatus {
-    PENDING, SALE, RESERVED, SOLD, REJECTED, DELETED
+    PENDING, SALE, RESERVED, SOLD, REJECTED, DELETED, EXPIRED
 }
